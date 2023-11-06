5392a7e2e8602012a271d49999a1cb912961de8e
DigKvs <diegokaviski14@gmail.com>
2023-11-06 01:07:39 -0300

    adjust the path of the imgs

--- src/components/footerComponents/Contact.jsx
+++ src/components/footerComponents/Contact.jsx
@@ -2,7 +2,7 @@
 const Contact = () => {
   return (
     <div className="bg-[#050505] p-6 h-full w-full grid grid-rows-3 md:grid-cols-2 md:grid-rows-4 justify-center items-center gap-10">
-        <img className="justify-self-center col-span-2 md:row-span-2 w-20" src="/imgs/star-1.svg" alt="starzada" />
+        <img className="justify-self-center col-span-2 md:row-span-2 w-20" src="/star-1.svg" alt="starzada" />
         <h2 className="text-[#FFD6FF] relative left-4 md:left-0 font-gothic text-2xl md:text-3xl col-span-2 justify-self-center p-5">Ready to team up with Diego? Your dream site awaits!</h2>
         <a href='https://w.app/mhRvNX' target="_blank" rel="noopener noreferrer" className="px-4 py-3 hover:opacity-80 bg-[#E8EDF0] justify-self-end rounded-lg font-playpen">Hire Diego</a>
         <a href="https://github.com/DigKvs" target="_blank" rel="noopener noreferrer" className="px-4 py-3 hover:opacity-80 bg-[#b8c0ff] justify-self-start rounded-lg font-playpen">View Github</a>
--- src/components/footerComponents/Social.jsx
+++ src/components/footerComponents/Social.jsx
@@ -4,15 +4,15 @@ const Social = () => {
     <div id="Contact" className=" bg-[#050505] w-full h-full flex flex-col justify-center items-center p-20">
         <div className=" w-24 grid grid-cols-2 justify-center items-center mb-10 md:mb-5 gap-6 ">
 
-            <a className="hover:opacity-80" href="https://github.com/DigKvs" target="_blank" rel="noopener noreferrer" ><img src="/imgs/github.svg" alt="github" /></a>
-            <a className="hover:opacity-80" href="https://www.linkedin.com/in/diego-kaviski-33a418252/" target="_blank" rel="noopener noreferrer" ><img src="/imgs/linkedin.svg" alt="linkedin" /></a>
+            <a className="hover:opacity-80" href="https://github.com/DigKvs" target="_blank" rel="noopener noreferrer" ><img src="/github.svg" alt="github" /></a>
+            <a className="hover:opacity-80" href="https://www.linkedin.com/in/diego-kaviski-33a418252/" target="_blank" rel="noopener noreferrer" ><img src="/linkedin.svg" alt="linkedin" /></a>
 
         </div>
-        <div className=" justify-center items-center flex relative m-2 gap-3 right-20 w-12 p-1 text-[#E8EDF0]" ><img src="/imgs/discord.svg" alt="discord" />dig7</div>
-        <div className="justify-center items-center flex m-2 gap-3 w-12 p-1 text-[#E8EDF0]" ><img src="/imgs/envelope-solid.svg" alt="email"/>diegokaviski14@gmail.com</div>
+        <div className=" justify-center items-center flex relative m-2 gap-3 right-20 w-12 p-1 text-[#E8EDF0]" ><img src="/discord.svg" alt="discord" />dig7</div>
+        <div className="justify-center items-center flex m-2 gap-3 w-12 p-1 text-[#E8EDF0]" ><img src="/envelope-solid.svg" alt="email"/>diegokaviski14@gmail.com</div>
 
         <div className="justify-center items-center flex relative right-10 m-2 gap-3 w-full p-1 text-[#E8EDF0]">
-          <img className="w-10" src="/imgs/phone-solid.svg" alt="phone"/>
+          <img className="w-10" src="/phone-solid.svg" alt="phone"/>
           <span className="flex h-full">(41)98786-8768
           </span>
         </div>
